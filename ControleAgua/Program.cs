2f70ffb6be7713273c9a27e87374c21fc9ab6146
Carol <carolduarte2000@gmail.com>
2024-10-03 11:24:37 -0300

    att

--- ControleAgua/Program.cs
+++ ControleAgua/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// false = não acionado, true = acionado
+bool boiaA,
+     boiaB,
+     boiaC,
+     bomba,
+     valvulaS2,
+     erro = false;
+
+int nivelAgua = 0;
+
+Console.Write("Informe o nível de água do reservatório: ");
+nivelAgua = int.Parse(Console.ReadLine());
+switch (nivelAgua)
+{
+    case 0:
+        bomba = false; // A bomba de água não pode funcionar com o reservatório vazio.
+        break;
+}
