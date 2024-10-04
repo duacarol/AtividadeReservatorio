be903a86c544aded68b3bff51334305b79d1cbe3
Carol <carolduarte2000@gmail.com>
2024-10-04 11:46:36 -0300

    Corrige a lógica de controle, melhora a interface e adiciona loop para novos testes

--- ControleAgua/Program.cs
+++ ControleAgua/Program.cs
@@ -1,40 +1,73 @@
-﻿bool boiaA = false,
+﻿// Tabela Verdade
+//
+// | A  | B  | C  | S1 | S2 | E  |
+// |----|----|----|----|----|----|
+// | 0  | 0  | 0  | 0  | 0  | 0  |
+// | 0  | 0  | 1  | 0  | 0  | 1  |
+// | 0  | 1  | 0  | 0  | 0  | 1  |
+// | 0  | 1  | 1  | 0  | 0  | 1  |
+// | 1  | 0  | 0  | 1  | 0  | 0  |
+// | 1  | 0  | 1  | 0  | 0  | 1  |
+// | 1  | 1  | 0  | 1  | 1  | 0  |
+// | 1  | 1  | 1  | 0  | 1  | 0  |
+
+void EscreverTitulo()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(@" _");
+    Console.WriteLine(@"( . __|_ _  _ _  _    _| _   |\/| _  _ ._|_ _  _ _  _ _  _  _ _|_ _   |_|´ _| _. _ _ ");
+    Console.WriteLine(@"_)|_\ | (/_| | |(_|  (_|(/_  |  |(_)| || | (_)| (_|| | |(/_| | | (_)  | ||(_|| |(_(_)");
+    Console.ResetColor();
+}
+
+inicio:
+
+bool boiaA = false,
      boiaB = false,
      boiaC = false,
      bomba = false,
      valvula = false,
      erro = false;
 
-// Leitura do estado das boias e dispositivos
-Console.WriteLine("Insira o estado das boias (0 para não acionada, 1 para acionada):");
+EscreverTitulo();
 
-Console.Write("Boia A (nível baixo): ");
+// Leitura do estado das boias
+Console.WriteLine("\nInforme o estado das boias (0 para \u001b[91mNÃO ACIONADA\u001b[0m, 1 para \u001b[92mACIONADA\u001b[0m):");
+
+Console.Write("> \u001b[36mBoia A\u001b[0m [Reservatório 1 – Nível 1]: ");
 boiaA = int.Parse(Console.ReadLine()) == 1;
 
-Console.Write("Boia B (nível intermediário): ");
+Console.Write("> \u001b[36mBoia B\u001b[0m [Reservatório 1 – Nível 2]: ");
 boiaB = int.Parse(Console.ReadLine()) == 1;
 
-Console.Write("Boia C (reservatório 2): ");
+Console.Write("> \u001b[36mBoia C\u001b[0m [Reservatório 2 – Nível 3]: ");
 boiaC = int.Parse(Console.ReadLine()) == 1;
 
 // Lógica para controle da bomba e da válvula com base no nível das boias
-if (!boiaA && !boiaB && !boiaC) // Nível baixo
+if (!boiaA && !boiaB && !boiaC) // Reservatórios vazios, tudo off
+{
+    erro = false;
+    bomba = false;
+    valvula = false;
+}
+else if (boiaA && !boiaB && !boiaC)
 {
     erro = false;
     bomba = true; // Liga a bomba
-    valvula = true; // Abre a válvula    
+    valvula = false;
 }
-else if (!boiaA && boiaB && !boiaC) // Nível intermediário
+else if (boiaA && boiaB && !boiaC)
 {
     erro = false;
     bomba = true; // Continua bombeando
-    valvula = false; // Fecha a válvula
+    valvula = true; // Abre a válvula
 }
-else if (boiaA && boiaB && boiaC) // Nível máximo
+else if (boiaA && boiaB && boiaC)
 {
     erro = false;
     bomba = false; // Desliga a bomba
-    valvula = false; // Fecha a válvula
+    valvula = true; // Válvula segue aberta
 }
 else // Inconsistência detectada
 {
@@ -43,26 +76,37 @@ else // Inconsistência detectada
     valvula = false; // Fecha a válvula
 }
 
+EscreverTitulo();
+
 // Exibindo o status do sistema
 if (erro)
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("\n|| ERRO: Inconsistência detectada! ||");
-    Console.WriteLine("|| Travando a bomba e a válvula! Chame a manutenção! ||");
+    Console.WriteLine("\n\u001b[91mERRO: Inconsistência detectada!");
+    Console.WriteLine("Travando a bomba e a válvula! Chame a manutenção!\u001b[0m");
 }
 else
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("\nSistema operando corretamente.");
-
+    Console.WriteLine("\n\u001b[92mSistema operando corretamente.");
     if (bomba)
-        Console.WriteLine("Bomba de água: LIGADA");
+        Console.WriteLine("\u001b[36mBomba de água: \u001b[92mLIGADA");
     else
-        Console.WriteLine("Bomba de água: DESLIGADA");
+        Console.WriteLine("\u001b[36mBomba de água: \u001b[91mDESLIGADA");
 
     if (valvula)
-        Console.WriteLine("Eletroválvula de entrada de água: ABERTA");
+        Console.WriteLine("\u001b[36mEletroválvula de entrada de água: \u001b[92mABERTA\u001b[0m");
     else
-        Console.WriteLine("Eletroválvula de entrada de água: FECHADA");
+        Console.WriteLine("\u001b[36mEletroválvula de entrada de água: \u001b[91mFECHADA\u001b[0m");
 }
-Console.ResetColor();
+
+Console.Write("\nDeseja fazer um novo teste? (S/N): ");
+string novoTeste = Console.ReadLine().ToUpper();
+if (novoTeste == "S")
+{
+    goto inicio;
+}
+else if (novoTeste == "N")
+{
+    EscreverTitulo();
+    Console.WriteLine("\nSaindo...");
+    return;
+}
